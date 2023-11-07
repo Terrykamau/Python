159b721206c3a1986addec415c58b5f886b02008
Terrykamau <150162826+Terrykamau@users.noreply.github.com>
2023-11-07 18:11:47 +0300

    Add files via upload
    my work

---
+++ start.py
@@ -0,0 +1,41 @@
+'''print("Hy")
+age=23
+if(age>=18):
+    print("Vote")
+else:
+    print("No voting")
+age =23
+age =23.3
+age = 28.3432435353
+age=16j
+age="thirteen"
+grade='A'
+ 
+food=["rice","pilau","chicken","Chapati"]
+ages=[19,78,12,43,87,34,25]
+#print(food)
+#print(ages)
+print(food+ages)'''
+print("hello Terry") 
+num=20
+num1=30
+#print(num+num1)
+#name=input("Enter your name: ") 
+#print(name)
+number1=float(input("Enter the first number: "))
+number2=float(input("Enter the second number: "))
+print((number1)+(number2))
+print((number1)*(number2))
+print((number1)-(number2))
+print((number1)/(number2))
+#kmeans=str(number1)+str(number2)
+#print(kmeans)
+
+state="""ydgefuee rwgjwrgojirwg rgjnwrogijr
+rgiwrhguirj
+rgiujgrwipi9oepfpo
+rjwugjrwkjgo
+krpgjiwrgkmw"""
+print(state)
+
+          
